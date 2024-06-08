904c67c9b71088423156aa3f398371167c9228f1
fuji-wei <foojunwei2003@gmail.com>
2024-06-08 18:37:55 +0800

    Added icon module

---
+++ constants/icons.js
@@ -0,0 +1,113 @@
+// bottom-tab-nav
+import categories from "../assets/images/bottom-tab-navigation/categories.png";
+import categoriesWhite from "../assets/images/bottom-tab-navigation/categories-white.png";
+import discover from "../assets/images/bottom-tab-navigation/categories-white.png";
+import discoverWhite from "../assets/images/bottom-tab-navigation/categories-white.png";
+import profile from "../assets/images/bottom-tab-navigation/categories-white.png";
+import profileWhite from "../assets/images/bottom-tab-navigation/categories-white.png";
+import home from "../assets/images/bottom-tab-navigation/categories-white.png";
+import homeWhite from "../assets/images/bottom-tab-navigation/categories-white.png";
+// categories
+import food from "../assets/images/categories/food.png";
+import homeAppliances from "../assets/images/categories/home-appliances.png";
+import menFashion from "../assets/images/categories/men-fashion.png";
+import womenFashion from "../assets/images/categories/women-fashion.png";
+// home
+import demoUser from "../assets/images/home/demo-user.png";
+import product from "../assets/images/home/product.png";
+import progressBar from "../assets/images/home/progress-bar.png";
+import search from "../assets/images/home/search.png";
+import seedling from "../assets/images/home/seedling.png";
+import shoppingCart from "../assets/images/home/shopping-cart.png";
+import user from "../assets/images/home/user.png";
+import voucher from "../assets/images/home/voucher.png";
+// check out
+import creditCard from "../assets/images/check-out/credit-card.png";
+import discount from "../assets/images/check-out/discount.png";
+import dollar from "../assets/images/check-out/dollar.png";
+import location from "../assets/images/check-out/location.png";
+import sustainability from "../assets/images/check-out/sustainability.png";
+// discover
+import article1 from "../assets/images/discover/article-1.png";
+import article2 from "../assets/images/discover/article-2.png";
+import author from "../assets/images/discover/author.png";
+import bookmarkOutline from "../assets/images/discover/bookmark-blank.png";
+import bookmarkFilled from "../assets/images/discover/bookmark2.png";
+// general
+import backArrow from "../assets/images/general/back.png";
+import dotGreen from "../assets/images/general/dot-green.png";
+import dotGrey from "../assets/images/general/dot-grey.png";
+import heartOutline from "../assets/images/general/heart-blank.png";
+import heartFilled from "../assets/images/general/heart.png";
+import leafGreen from "../assets/images/general/leaf-green.png";
+import leafGrey from "../assets/images/general/leaf-grey.png";
+import starYellow from "../assets/images/general/star-yellow.png";
+import starGrey from "../assets/images/general/star-grey.png";
+// profile
+import logout from "../assets/images/profile/logout.png";
+import orderHistory from "../assets/images/profile/order-history.png";
+import orderProgress from "../assets/images/profile/order-progress.png";
+import rank from "../assets/images/profile/rank.png";
+import bin from "../assets/images/profile/trash.png";
+// product description
+import minus from "../assets/images/product-description/minus.png";
+import plus from "../assets/images/product-description/plus.png";
+import storeProfile from "../assets/images/product-description/store-profile.png";
+import product2 from "../assets/images/product-description/product.png";
+// search
+import toy from "../assets/images/search/toy.png";
+// shopping cart
+import leaf0 from "../assets/images/shopping-cart/eco rating 0.png";
+import leaf1 from "../assets/images/shopping-cart/eco rating 1.png";
+import leaf2 from "../assets/images/shopping-cart/eco rating 2.png";
+import leaf3 from "../assets/images/shopping-cart/eco rating 3.png";
+import leaf4 from "../assets/images/shopping-cart/eco rating 4.png";
+import leaf5 from "../assets/images/shopping-cart/eco rating 5.png";
+import leaf6 from "../assets/images/shopping-cart/eco rating 6.png";
+import leaf7 from "../assets/images/shopping-cart/eco rating 7.png";
+import leaf8 from "../assets/images/shopping-cart/eco rating 8.png";
+import leaf9 from "../assets/images/shopping-cart/eco rating 9.png";
+import leaf10 from "../assets/images/shopping-cart/eco rating 10.png";
+import orderSuccess from "../assets/images/shopping-cart/order-success.png";
+import leftArrow from "../assets/images/shopping-cart/left-arrow.png";
+import rightArrow from "../assets/images/shopping-cart/right-arrow.png";
+import tick from "../assets/images/shopping-cart/tick.png";
+// sign in
+import lock from "../assets/images/sign-in/lock.png";
+import userProfile from "../assets/images/sign-in/user.png";
+// splash screen
+import logo from "../assets/images/splash-screen/logo.png";
+import bg from "../assets/images/splash-screen/splash-bg.png";
+// store-page
+import storeRating from "../assets/images/store-page/store-rating.png";
+
+
+
+export default {
+  //bottomTabNav
+  categories, categoriesWhite, discover, discoverWhite, profile, profileWhite, home, homeWhite,
+  //categories
+  food, homeAppliances, menFashion, womenFashion,
+  //home
+  demoUser, product, progressBar, search, seedling, shoppingCart, user, voucher,
+  //checkOut
+  creditCard, discount, dollar, location, sustainability,
+  //discover
+  article1, article2, author, bookmarkFilled, bookmarkOutline,
+  //general
+  backArrow, dotGreen, dotGrey, heartFilled, heartOutline, leafGreen, leafGrey, starYellow, starGrey,
+  //profile
+  logout, orderHistory, orderProgress, rank, bin,
+  //productDescription
+  minus, plus, product2, storeProfile,
+  //signIn
+  lock, userProfile,
+  //cart
+  leaf0, leaf1, leaf2, leaf3, leaf4, leaf5, leaf6, leaf7, leaf8, leaf9, leaf10, leftArrow, rightArrow, tick, orderSuccess,
+  //splashScreen
+  logo, bg,
+  //storePage
+  storeRating,
+  //search
+  toy
+};
