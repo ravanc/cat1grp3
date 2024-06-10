07f691e7cf3d7095f84582bb9eff39d3d98609cc
Ravan <ravan2003@gmail.com>
2024-06-10 11:08:45 +0800

    Cleaned up discover page and components

--- app/(tabs)/discover.jsx
+++ app/(tabs)/discover.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Image, TouchableOpacity, FlatList, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import { View, Image, TouchableOpacity, FlatList, SafeAreaView } from 'react-native'
+import React from 'react'
 import SearchBar from '../../components/discover/searchbar'
 import ArticleCard from '../../components/discover/article-card'
 
@@ -10,55 +10,63 @@ const Discover = () => {
       title: '10 Latest Sustainability Efforts By Fashion Brands In Asia',
       image: require('../../assets/images/discover/article-1.png'),
       date: '4 March 2024',
-      duration: '15 min read'
+      duration: '15 min read',
+      route: '/article1',
+      id: 1,
     },
     {
       category: 'Shopping',
       title: '5 Tips On How To Save While Shopping Sustainably',
       image: require('../../assets/images/discover/article-2.png'),
       date: '4 March 2024',
-      duration: '10 min read'
+      duration: '10 min read',
+      route: '/article2',
+      id: 2,
     },
     {
       category: 'Fashion',
       title: '10 Latest Sustainability Efforts By Fashion Brands In Asia',
       image: require('../../assets/images/discover/article-1.png'),
       date: '4 March 2024',
-      duration: '15 min read'
+      duration: '15 min read',
+      route: '/article3',
+      id: 3,
     },
     {
       category: 'Fashion',
       title: '10 Latest Sustainability Efforts By Fashion Brands In Asia',
       image: require('../../assets/images/discover/article-1.png'),
       date: '4 March 2024',
-      duration: '15 min read'
+      duration: '15 min read',
+      route: '/article4',
+      id: 4,
     }
   ]
   return (
-    <View className='flex items-center mt-14'>
-      <View className='flex-row items-center mb-5'>
+    <SafeAreaView className='flex items-center h-full bg-white'>
+      <View className='flex-row items-center mb-2'>
         <SearchBar/>
-        <TouchableOpacity onPress={() => {console.log('go to bookmarks')}}>
+        <TouchableOpacity onPress={() => {console.log('go to bookmarks')}} activeOpacity={0.9}>
           <Image source={require('../../assets/images/discover/bookmark-blank.png')} className='ml-3'/>
         </TouchableOpacity>
       </View>
       <FlatList
         data={articles}
-        renderItem={({item, index}) =>
+        renderItem={({item}) =>
           <ArticleCard
             category={item.category}
             title={item.title}
             image={item.image}
             date={item.date}
             duration={item.duration}
+            route={item.route}
+            key={item.id}
           />
         }
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{paddingTop: 10, paddingBottom: 70}}
       />
-      
-      <ArticleCard/>
-    </View>
+    </SafeAreaView>
   )
 }
 
--- components/discover/article-card.jsx
+++ components/discover/article-card.jsx
@@ -1,33 +1,37 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, TouchableHighlight } from 'react-native'
 import React from 'react'
 import ArticleTag from './article-tag'
 import { useState } from 'react';
 
-const ArticleCard = ({ category, title, image, date, duration }) => {
+const ArticleCard = ({ category, title, image, date, duration, route }) => {
     const [isBookmarked, setIsBookmarked] = useState(false);
 
   return (
-        <View className='h-auto w-[85vw] rounded-3xl overflow-hidden mb-3 border-[#dedddd] border-2'>
-            <Image source={image} className='h-40 w-full '/>
-            <View className='flex-column h-auto mt-2 px-3'>
-                <View className='flex-row items-center'>
-                    <ArticleTag category={category}/>
-                    <View className='ml-auto'>
-                        <TouchableOpacity onPress={() => {setIsBookmarked(!isBookmarked)}}>
-                            <Image source={!isBookmarked ? require('../../assets/images/discover/bookmark-blank.png') : require('../../assets/images/discover/bookmark2.png')}/>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                
-                <Text className='font-psemibold text-[14px] text-wrap'>{title}</Text>
-                
-                <View className='flex-row mb-1'>
-                    <Text className='font-pitalic text-[12px] text-[#979797]'>{duration}</Text>
-                    <Text className='font-pitalic text-[12px] text-[#979797] ml-auto'>{date}</Text>
-                </View>
-            </View>
+    <TouchableHighlight
+      onPress={() => console.log('push to', route)}
+      className='h-auto w-[85vw] rounded-3xl overflow-hidden mb-5 border-[#dedddd] border-2'
+      underlayColor={'#DDDDDD'}
+      activeOpacity={0.9}
+    >
+      <View>
+      <Image source={image} className='h-40 w-full '/>
+      <View className='flex-column h-auto px-3 py-2'>
+        <View className='flex-row items-center'>
+        <ArticleTag category={category}/>
+        <View className='ml-auto'>
+          <TouchableOpacity onPress={() => {setIsBookmarked(!isBookmarked)}} activeOpacity={0.9}>
+            <Image source={!isBookmarked ? require('../../assets/images/discover/bookmark-blank.png') : require('../../assets/images/discover/bookmark2.png')}/>
+          </TouchableOpacity>
         </View>
-      
+        </View>
+        <Text className='font-psemibold text-[14px] text-wrap mt-1'>{title}</Text>
+        <View className='flex-row my-1'>
+          <Text className='font-pitalic text-[12px] text-[#979797]'>{duration}</Text>
+          <Text className='font-pitalic text-[12px] text-[#979797] ml-auto'>{date}</Text>
+        </View>
+      </View>
+      </View>  
+    </TouchableHighlight>
   )
 }
 
